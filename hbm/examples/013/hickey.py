f26cea09570c842e24d712d99af51ede3a8dd674
permas4edu <129726439+permas4edu@users.noreply.github.com>
2023-11-16 18:11:28 +0100

    Add files via upload

---
+++ hbm/examples/013/hickey.py
@@ -0,0 +1,23 @@
+import os, sys
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+csvs = glob.glob('hbm_*_u.csv')
+csvs.sort()
+
+fig = plt.figure('HBM')
+ax = fig.add_subplot(111)
+label = [-0.003,0.04]
+for i, csv in enumerate(csvs):
+    df = pd.read_csv(csv,delimiter=';')    
+    ax.plot(2*np.pi*df['Frequency'],df['H1-N101,u'],label=r'$\lambda=%+.3f$' %label[i])
+ax.set_xlabel(r'$\Omega$')
+ax.set_ylabel(r'$\max(q_1)$')
+ax.set_xlim(0.0,2.5)
+ax.set_xticks(np.linspace(0.0,2.5,6))
+ax.grid()
+ax.legend(shadow=True).set_draggable(True)
+plt.tight_layout()
+plt.show()
