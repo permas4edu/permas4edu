b55be01f54d12df288e79b7af10f7eceb631d714
permas4edu <129726439+permas4edu@users.noreply.github.com>
2023-11-16 18:19:05 +0100

    Add files via upload

--- hbm/examples/013/hickey.py
+++ hbm/examples/013/hickey.py
@@ -13,8 +13,8 @@ label = [-0.003,0.04]
 for i, csv in enumerate(csvs):
     df = pd.read_csv(csv,delimiter=';')    
     ax.plot(2*np.pi*df['Frequency'],df['H1-N101,u'],label=r'$\lambda=%+.3f$' %label[i])
-ax.set_xlabel(r'$\Omega$')
-ax.set_ylabel(r'$\max(q_1)$')
+ax.set_xlabel(r'$\frac{\Omega}{\omega_n}$')
+ax.set_ylabel(r'$u_1$')
 ax.set_xlim(0.0,2.5)
 ax.set_xticks(np.linspace(0.0,2.5,6))
 ax.grid()
