871bad35f030ec9e36085d8d9ddccd7e03381f49
permas4edu <129726439+permas4edu@users.noreply.github.com>
2023-12-12 11:55:02 +0100

    Simplified mass-spring system model for reticulated shells

---
+++ dev/examples/021/liu_02.py
@@ -0,0 +1,22 @@
+import os, sys
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+mpl.rcParams['savefig.directory']=os.getcwd()
+import matplotlib.pyplot as plt
+df1 = pd.read_csv('sampling_xdhis.csv',delimiter=';')
+df2 = pd.read_csv('sampling_srhis.csv',delimiter=';')
+fig = plt.figure('Veering')
+ax = fig.add_subplot(111)
+ax.plot(df1['DV_1'],2*np.pi*df2['F_1'],label=r'$\omega_1$')
+ax.plot(df1['DV_1'],2*np.pi*df2['F_2'],label=r'$\omega_2$')
+ax.plot(df1['DV_1'],2*np.pi*df2['F_3'],label=r'$\omega_3$')
+ax.grid('on')
+ax.legend(shadow=True).set_draggable(True)
+ax.set_xlabel(r'$k_{c2}$')
+ax.set_ylabel(r'Frequency $\omega=2\pi\,f$')
+ax.set_yticks(np.linspace(0.0,6.0,7))
+ax.set_xticks(np.linspace(0.0,15.,6))
+ax.set_xlim(0.,15.)
+plt.tight_layout()
+plt.show()
