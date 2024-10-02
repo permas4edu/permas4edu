1292878af89c7d25dc9c118adea3d075aad9a89d
permas4edu <129726439+permas4edu@users.noreply.github.com>
2024-10-02 09:54:25 +0200

    Update tmd.py

--- dra/examples/013/tmd.py
+++ dra/examples/013/tmd.py
@@ -16,7 +16,7 @@ for i, csv in enumerate(csvs):
 #    ax.plot(2*np.pi*df['Frequency'],df['N102,u'],label=r'$x_2$')
 ax.grid()
 ax.set_ylim(0.0,3.5E-5)
-ax.set_xlabel(r'Frquency $\Omega$ [rad/s]')
+ax.set_xlabel(r'Frequency $\Omega$ [rad/s]')
 ax.legend(shadow=True).set_draggable(True)
 ax.set_xticks(np.linspace(0.0,180.,10))
 plt.tight_layout()
