743df130ddbf8725e36783aef1811115336e6202
permas4edu <129726439+permas4edu@users.noreply.github.com>
2025-05-06 10:49:01 +0200

    Single-degree-of-freedom oscillator with slight asymmetry in the clearance

---
+++ hbm/examples/046/chengwu.py
@@ -0,0 +1,21 @@
+import os, sys
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+mpl.rcParams['savefig.directory']=os.getcwd()
+import matplotlib.pyplot as plt
+fig = plt.figure('HBM_03')
+ax = fig.add_subplot(111)
+csv = sys.argv[1]
+df = pd.read_csv(csv,delimiter=';')
+ax.plot(2*np.pi*df['Frequency'],df['H1-N101,u'],lw=2,label='PERMAS')
+ax.legend(shadow=True).set_draggable(True)
+#ax.set_xlim(1.0,2.5)
+ax.set_xlim(0.0,2.5)
+#ax.set_xticks(np.linspace(1.0,2.5,4))
+ax.set_xticks(np.linspace(0.0,2.5,6))
+ax.set_xlabel(r'Frequency $\Omega$ [rad/s]')
+ax.set_ylabel(r'$\hat{x}$')
+ax.grid()
+plt.tight_layout()
+plt.show()
