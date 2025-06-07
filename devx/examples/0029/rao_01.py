314bf2a3a7df9f181c45ae6489fef30ae8ffb444
permas4edu <129726439+permas4edu@users.noreply.github.com>
2025-06-07 13:52:53 +0200

    Rao’s coaxial-rotor system

--- devx/examples/0029/rao_01.py
+++ devx/examples/0029/rao_01.py
@@ -4,16 +4,27 @@ import pandas as pd
 import matplotlib as mpl
 mpl.rcParams['savefig.directory']=os.getcwd()
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator
+
 csv = sys.argv[1]
 df = pd.read_csv(csv,delimiter=';')
 fig = plt.figure('Campbell')
 ax=fig.add_subplot(111)
-for i in range(5):
+for i in range(9):
     ax.plot(2*np.pi*df['Rot.Freq.'],2*np.pi*df[df.keys()[2*i+1]],label=r'Mode $i$=%i' %(i+1))
 ax.grid()
 ax.plot(2*np.pi*df['Rot.Freq.'],2*np.pi*df['Rot.Freq.'],'-.',c='k',alpha=0.4)
 ax.set_xlabel('Inner rotor spin speed [rad/s]')
-ax.set_ylabel('Whirl speed')
-ax.legend(shadow=True).set_draggable(True)
+ax.set_ylabel('Whirl speed [rad/s]')
+ax.set_yticks(np.linspace(0.,3000.,7))
+ax.set_xticks(np.linspace(0.,2500.,6))
+ax.set_ylim(0.,3000.)
+ax.set_xlim(0.,2500.)
+ax.xaxis.set_major_locator(MultipleLocator(500))
+ax.xaxis.set_minor_locator(MultipleLocator(100))
+ax.yaxis.set_minor_locator(MultipleLocator(100))
+ax.legend(shadow=True,bbox_to_anchor=(1.1, 1.05)).set_draggable(True)
+ax.xaxis.grid(True, which='minor')
+ax.yaxis.grid(True, which='minor')
 plt.tight_layout()
 plt.show()
