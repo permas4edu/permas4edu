382f71ee98ec6927ec5bd38687da11c2408a3097
permas4edu <129726439+permas4edu@users.noreply.github.com>
2025-08-21 15:24:55 +0200

    Add files via upload

--- devx/examples/0032/hueseyin.py
+++ devx/examples/0032/hueseyin.py
@@ -1,21 +1,28 @@
-import os,sys
+import os, sys
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
 mpl.rcParams['savefig.directory']=os.getcwd()
 import matplotlib.pyplot as plt
+
 csv = sys.argv[1]
+
 df = pd.read_csv(csv,delimiter=';')
+
 fig = plt.figure('Campbell')
 ax = fig.add_subplot(111)
-ax.plot(df['Rot.Freq.'],df['Mode 1'],label='BW-1')
-ax.plot(df['Rot.Freq.'],df['Mode 3'],label='FW-1')
-ax.plot(df['Rot.Freq.'],df['Mode 5'],label='BW-2')
-ax.plot(df['Rot.Freq.'],df['Mode 7'],label='FW-2')
-ax.set_xlabel('Spin speed [1/s]')
-ax.set_ylabel(r'Frequency $f$ [Hz]')
+ax.plot(60*df['Rot.Freq.'],df[df.keys()[1]],lw=2,label='BW-1')
+ax.plot(60*df['Rot.Freq.'],df[df.keys()[3]],lw=2,label='FW-1')
+ax.plot(60*df['Rot.Freq.'],df[df.keys()[7]],lw=2,label='BW-2')
+ax.plot(60*df['Rot.Freq.'],df[df.keys()[5]],lw=2,label='FW-2')
+ax.plot(60*df['Rot.Freq.'],df['Rot.Freq.'],c='k',alpha=0.5,label='1X RPM')
 ax.grid()
-ax.set_xlim(0.,1000.)
+ax.set_xlabel('Rotational speed $\Omega$ [rpm]')
+ax.set_ylabel('Frequency $f$ [Hz]')
 ax.legend(shadow=True).set_draggable(True)
+ax.set_xlim(0.,18000.)
+ax.set_ylim(0.,350.)
+ax.set_xticks(np.linspace(0.,18000.,10))
+ax.set_yticks(np.linspace(0.,350.,8))
 plt.tight_layout()
 plt.show()
