cc68b7637ca16705aa5917026ee2078efd14e9e3
permas4edu <129726439+permas4edu@users.noreply.github.com>
2025-10-17 19:51:15 +0200

    Compliant Support Fixture

---
+++ sampling/examples/033/torque_02.py
@@ -0,0 +1,27 @@
+import os, sys
+import glob
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+csvs = glob.glob('angle_*_angle.csv')
+fig = plt.figure('Twist angle')
+ax = fig.add_subplot(111)
+k = [2.500000E+04, 5.000000E+04, 1.000000E+05, 1.000000E+12]
+
+for i, csv in enumerate(csvs):
+    df = pd.read_csv(csv, delimiter = ';')
+
+    ax.plot(df['Coor_X'], np.abs(df['LPAT_1_phiu']),label=r'$k_%i=%e$' %(i+1,k[i]))
+
+ax.grid()
+ax.set_xlabel(r'$x$ [m]')
+ax.set_ylabel(r'$\Phi$ [rad]')
+ax.set_ylim(0.,0.3)
+ax.set_xlim(0.,1.4)
+ax.set_yticks(np.linspace(0.,0.3,7))
+ax.legend(shadow=True).set_draggable(True)
+plt.tight_layout()
+plt.show()
