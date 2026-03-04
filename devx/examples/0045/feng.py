03e0c7cc0ec312e304329eccad293461d4421b5f
permas4edu <129726439+permas4edu@users.noreply.github.com>
2026-03-04 08:50:05 +0100

    Add files via upload

--- devx/examples/0045/feng.py
+++ devx/examples/0045/feng.py
@@ -1,10 +1,24 @@
+import os, sys
+import pandas as pd
 import numpy as np
-di=0.01
-do=0.039
-m1=0.67
-m2=0.5
-rho=7.8E3
-t1=4*m1/(rho*np.pi*(do**2-di**2))
-t2=4*m2/(rho*np.pi*(do**2-di**2))
-print ('t1=',t1)
-print ('t2=',t2)
+import matplotlib as mpl
+mpl.rcParams['savefig.directory']=os.getcwd()
+import matplotlib.pyplot as plt
+df = pd.read_csv('campbell_rsfrq.csv',delimiter=';')
+fig = plt.figure('Campbell diagram')
+ax = fig.add_subplot(111)
+ax.plot(df['Rot.Freq.'],df['Mode 1'],label='BW-1')
+ax.plot(df['Rot.Freq.'],df['Mode 3'],label='FW-1')
+ax.plot(df['Rot.Freq.'],df['Mode 5'],label='BW-2')
+ax.plot(df['Rot.Freq.'],df['Mode 7'],label='FW-2')
+ax.plot(df['Rot.Freq.'],df['Mode 9'],label='FW-3')
+ax.plot(df['Rot.Freq.'],df['Mode 11'],label='BW-3')
+ax.plot(df['Rot.Freq.'],df['Mode 13'],label='BW-4')
+ax.plot(df['Rot.Freq.'],df['Mode 15'],label='FW-4')
+ax.plot(df['Rot.Freq.'],df['Rot.Freq.'],c='k',alpha=0.5)
+ax.set_xlabel('Rotational speed [Hz]')
+ax.set_ylabel(r'Frequency $f$ [Hz]')
+ax.grid()
+ax.legend(shadow=True).set_draggable(True)
+plt.tight_layout()
+plt.show()
