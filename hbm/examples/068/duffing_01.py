4ce15fbd062f90724dd335baad872e1d7b61e05e
permas4edu <129726439+permas4edu@users.noreply.github.com>
2026-04-29 07:23:57 +0200

    Duffing oscillator

---
+++ hbm/examples/068/duffing_01.py
@@ -0,0 +1,26 @@
+import os, sys
+import matplotlib as mpl
+mpl.rcParams['savefig.directory'] = os.getcwd()
+#mpl.rcParams['xtick.minor.visible'] = True
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from matplotlib.ticker import AutoMinorLocator
+import pandas as pd
+import numpy as np
+csv1 = sys.argv[1]
+df1 = pd.read_csv(csv1,delimiter=';')
+fig = plt.figure('HBM',figsize=(5,5))
+ax1 = fig.add_subplot(111)
+ax1.plot(2*np.pi*df1['Frequency'],df1['H1-N101,u'],c='b',lw=2,label=r'$H_1@x_1$')
+ax1.grid()
+ax1.set_xlabel(r'Excitation Frequency $\Omega$ ')
+ax1.set_xlim(0.5,1.6)
+
+ax1.set_xticks(np.linspace(0.6,1.6,6))
+ax1.set_yticks(np.linspace(0.0,3.5,8))
+ax1.set_ylabel(r'Amplitude $C$')
+ax1.legend(shadow=True).set_draggable(True)
+plt.gca().xaxis.set_minor_locator(AutoMinorLocator(2))
+plt.gca().yaxis.set_minor_locator(AutoMinorLocator(2))
+plt.tight_layout()
+plt.show()
